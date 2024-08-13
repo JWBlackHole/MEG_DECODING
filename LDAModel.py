73ec97eb41a3a9d195805b680f2e4de49ba13812
taliah-hl <mhlee.c@zohomail.eu>
2024-08-13 16:43:18 +0800

    dev: allow LDA to predict for each timepoint or each window, not yet tested

--- LDAModel.py
+++ LDAModel.py
@@ -20,9 +20,25 @@ class MyLDA():
     def __init__(self):
         pass
 
-    def decode_binary(self, X: np.ndarray, y: np.ndarray, meta: pd.DataFrame, times= None, to_print_csv = False):
+    def decode_binary(self, X: np.ndarray, y: np.ndarray, meta: pd.DataFrame, times= None, 
+                      to_print_csv = False,
+                      prediction_mode = "collapse"):
         """
         y is expected to be binary with value True or False
+
+        Parameters
+        -----------
+        X (np.ndarray)
+            shape (n_window, n_chans, n_times)
+        y (np.ndarray)
+            shape (n_window,)
+        meta (pd.DataFrame)
+        to_print_csv (bool): 
+            - if True: save prediction resultsas a csv file
+        prediction_mode (str): Mode of prediction. Available options are:
+            - "collapse": Predict a single probability for each window
+            - "each_timepoint": Predict a probability for each time point in each window
+
         """
         logger.info("decod_binary is running")
         try:
@@ -36,71 +52,173 @@ class MyLDA():
         logger.debug(f"meta.shape: {meta.shape}")
         meta = meta.reset_index()
 
-        # Initialize the scaler and the LDA model
-        scaler = StandardScaler()
+        # convert y to correct dtype before LDA
+        y = np.array(y, dtype = bool)   # LDA expects y to be np arr with dtype numeric or bool
+
+
+        pred_df = scores = None
+
+        # call function for LDA according to prediction_mode
+        if (prediction_mode == "each_timepoint"):
+            pred_df, scores = self.predict_each_timepoint(X, y, meta)
+        elif (prediction_mode == "each_timepoint"):
+            self.predict_each_window(X, y, meta)
+        
+        if to_print_csv:
+            pass # TBC
+
+        return pred_df, scores
+
+    def predict_each_timepoint(self, X: np.ndarray, y: np.ndarray, meta: pd.DataFrame):
+        """
+        predict a probability for each time point in each window
+        Parameters
+        -----------
+        X (np.ndarray)
+            shape (n_window, n_chans, n_times)
+        y (np.ndarray)
+            shape (n_window,)
+        meta (pd.DataFrame)
+        """
+        # Initialize the LDA model
         lda = LinearDiscriminantAnalysis()
 
-        y = np.array(y, dtype = bool)
+        # Initialize return values
+        pred_df = None
+        ret_scores = None
+
+        if len(X.shape) != 3    # assert X is 3D array
+            logger.error(f"X is not 3D array, X.shape={X.shape}, non 3D X is not suported yet")
+            return pred_df, ret_scores
+        if not (y.dtype == bool or np.issubdtype(y.dtype, np.number)):
+            logger.error(f"LDA expects y in dtype bool or numeric only, y.dtype={y.dtype}")
+            return pred_df, ret_scores
+        # scaler = StandardScaler()         
 
         # Split the data into training and test sets
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
         scores = []
-        #y_train = np.array(y_train, dtype=bool)     # convert y to format lda model expect
-        # which needs to be np arr with dtype numeric or bool
         
         n, nchans, ntimes = X.shape
-        preds = np.zeros((len(X_test), ntimes, 2))  # Adjusted to store probabilities for both classes
-        pred = None # tmp pred for one time point
-        df = None # tmp df for one time point
+        preds = np.zeros((n, ntimes))
+
 
         # predict by my self
         for t in range(2):
-            # Scale the data
+            # Scale the data (seems no need)
             # X_train_scaled = scaler.fit_transform(X_train[:, :, t])
             # X_test_scaled = scaler.transform(X_test[:, :, t])
 
-            # Fit the LDA model
-            logger.debug(f"shape of X_train[:, :, t]:  {X_train[:, :, t].shape}")
-            logger.debug(f"shape of y_train: {y_train.shape}")
-            lda.fit(X_train[:, :, t], y_train)
-            logger.info(f'n_components: {lda.n_components}')
+            
+            if(t==0):   # only print debug message in 1st interation
+                logger.debug(f"shape of X_train[:, :, t]:  {X_train[:, :, t].shape}")
+                logger.debug(f"shape of y_train: {y_train.shape}")
 
+            # Fit the LDA model
+            lda.fit(X_train[:, :, t], y_train)
+
+            if(t==0):   # only print debug message in 1st interation
+                logger.info(f'n_components: {lda.n_components}')           
+                logger.debug(f"shpae of X_test: {X_test[:, :, t].shape}")
 
             # Predict probabilities by projected X form the fitted LD
-            logger.debug(f"shpae of X_test: {X_test[:, :, t].shape}")
-            pred = lda.predict(X_test[:, :, t])
+            preds[:, t] = lda.predict(X_test[:, :, t])
 
-            # add code to make a df of preds
-            df = pd.DataFrame(pred, columns=["pred"])
-            df["ground_truth"] = y_test.tolist()
+        pred_df = pd.DataFrame(preds, columns=[f"timepoint_{t}" for t in range(ntimes)])
+        pred_df["ground_truth"] = y_test.tolist()   # there is only one column of y because all timepoints correspond to same event
             
         
         # below still need debug
             
         # evaluate by k fold
 
-        ret2 = None
+        ret_socres = None
 
         try:
-            for t in range(1):
+            for t in range(2):
                 cv = RepeatedStratifiedKFold(n_splits=5, n_repeats = 3, random_state=1)
                 # n_split = no. of folds
                 score = cross_val_score(lda, X[:, :, t], y, scoring="accuracy", cv=cv, n_jobs=-1)
                 scores.append(score)
-                ret2 = scores
+                ret_socres = scores
                 try:
                     scores_df = pd.DataFrame(scores, columns=[f"fold_{i}" for i in range(len(scores))])
-                    ret2 = scores_df
+                    ret_socres = scores_df
                 except Exception:
                     logger.error("error occur in converting scores to df")
-                    # add code to convert scores to df
                     
         except Exception:
             logger.error("error occur in get kfold score, returning None for scores")
-            # add code to convert scores to df
-            
+            return pred_df, None
+        return pred_df, ret_socres
+    
+    def predict_each_window(X, y, meta):
+        """
+        predict a probability for each window (collapse all timepoints in each window)
 
-            return df, None
+        Parameters
+        -------------
+        X (np.ndarray)
+            shape (n_window, n_chans, n_times)
+        y (np.ndarray)
+        """
+        # Initialize the LDA model
+        lda = LinearDiscriminantAnalysis()
+
+        # Initialize return values
+        pred_df = None
+        ret_socres = None
+
+        # Check if X is a 3D array
+        if len(X.shape) != 3:
+            logger.error(f"X is not a 3D array, X.shape={X.shape}, not capable for predict_each_window")
+            return pred_df, ret_socres
+
+        # Check if y is a numpy array of dtype bool or numeric, as this is expected by LDA
+        if not (y.dtype == bool or np.issubdtype(y.dtype, np.number)):
+            logger.error(f"LDA expects y in dtype bool or numeric only, y.dtype={y.dtype}")
+            return pred_df, ret_socres
+
+        n, nchans, ntimes = X.shape
+        X = X.reshape(-1, nchans * ntimes)     # collapse nchans and ntimes to 1D
+        # should be same as X.reshape(n, -1) [TBC]
 
 
-        return df, ret2
+        # Split the data into training and test sets
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+        scores = []        
+        
+        
+        preds = np.zeros(n)
+        logger.debug(f"shape of X_train: {X_train.shape}")
+        logger.debug(f"shape of y_train: {y_train.shape}")
+
+        lda.fit(X_train, y_train)
+
+        logger.debug(f"shape of X_test: {X_test.shape}")
+        lda.predict(X_test)
+
+        # construct df from prediction result
+        pred_df = pd.DataFrame(preds, columns=["prediction"])
+        pred_df["ground_truth"] = y_test.tolist()
+
+
+        try:
+
+            cv = RepeatedStratifiedKFold(n_splits=5, n_repeats = 3, random_state=1)
+            # n_split = no. of folds
+            score = cross_val_score(lda, X, y, scoring="accuracy", cv=cv, n_jobs=-1)
+            scores.append(score)
+            ret_socres = scores
+            try:
+                scores_df = pd.DataFrame(scores, columns=[f"fold_{i}" for i in range(len(scores[0]))])
+                ret_socres = scores_df
+            except Exception:
+                logger.error("error occur in converting scores to df")
+                return pred_df, ret_socres
+                
+        except Exception:
+            logger.error("error occur in get kfold score, returning None for scores")
+            return pred_df, None
+        
+        return pred_df, ret_socres
