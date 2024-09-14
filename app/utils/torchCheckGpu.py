1fbeea660e394943631ecfbfc26db37f61da124f
taliah-hl <taliah-1111@protonmail.com>
2024-09-14 11:03:57 +0000

    move check gpu

--- app/utils/torchCheckGpu.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-if __name__=="__main__":
-        
-    if torch.cuda.is_available():
-        print(f"GPU: {torch.cuda.get_device_name(0)} is available")
-    else:
-        print("no GPU!")
