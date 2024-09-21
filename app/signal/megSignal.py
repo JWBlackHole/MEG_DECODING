612764b3c0a9192d7ea9a3c5ea5ba66ff4ab5c22
taliah-hl <mhlee.c@zohomail.eu>
2024-09-21 20:42:49 +0800

    revert

--- app/signal/megSignal.py
+++ app/signal/megSignal.py
@@ -240,8 +240,8 @@ class MEGSignal():
         #events
         # 1st col: onset time
 
-        #if self.preload:    # this cannot be done if preload==False, as data is not loaded
-        if False:
+        if self.preload:    # this cannot be done if preload==False, as data is not loaded
+        #if False:
             # threshold
             th = np.percentile(np.abs(epochs._data), 95)
             epochs._data[:] = np.clip(epochs._data, -th, th)
