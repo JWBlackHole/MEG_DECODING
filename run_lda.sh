21b60d046ea872100cb74d9861d6517df7ed6349
taliah-hl <mhlee.c@zohomail.eu>
2024-09-22 22:51:39 +0800

    delete some old config and update

--- run_lda.sh
+++ run_lda.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# Loop through numbers 1 to 27
-for i in {1..27}
+# Array of subfolders from 'i' to 'k'
+folders=(k)
+
+# Loop through each folder
+for folder in "${folders[@]}"
 do
-    # Run the Python script with the current config file
-    python main.py -o ./app/config/lda/lda_$i.json
-    
-    # Wait for 2 seconds before the next iteration
-    sleep 2
+    # Loop through numbers 1 to 4
+    for i in {1..4}
+    do
+        # Run the Python script with the current config file from the subfolder
+        python main.py -o ./app/config/lda/0922/$folder/lda_$i.json
+
+        # Wait for 2 seconds before the next iteration
+        sleep 2
+    done
 done
