152cabb49b90a98439424aa1c33e7b05cf2f6fa4
taliah-hl <taliah-1111@protonmail.com>
2024-09-22 08:09:48 +0000

    update

--- run_lda.sh
+++ run_lda.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# Loop through numbers 1 to 27
-for i in {1..27}
+# Array of subfolders from 'a' to 'h'
+folders=(a b c d f g h)
+
+# Loop through each folder
+for folder in "${folders[@]}"
 do
-    # Run the Python script with the current config file
-    python main.py -o ./app/config/lda/lda_$i.json
-    
-    # Wait for 2 seconds before the next iteration
-    sleep 2
+    # Loop through numbers 1 to 27
+    for i in {1..27}
+    do
+        # Run the Python script with the current config file from the subfolder
+        python main.py -o ./app/config/lda/0921/$folder/lda_$i.json
+
+        # Wait for 2 seconds before the next iteration
+        sleep 2
+    done
 done
