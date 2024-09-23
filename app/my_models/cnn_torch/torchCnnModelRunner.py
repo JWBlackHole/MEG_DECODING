d7421bae3ab57cdf8b497988a148969a407c5e32
taliah-hl <mhlee.c@zohomail.eu>
2024-09-23 18:08:17 +0800

    correct the structure of cnn model runner (not tested)

--- app/my_models/cnn_torch/torchCnnModelRunner.py
+++ app/my_models/cnn_torch/torchCnnModelRunner.py
@@ -1,7 +1,9 @@
 import torch
 import torch.nn as nn
+from torch import Tensor
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset, random_split
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
 import pandas as pd
@@ -12,6 +14,10 @@ import random
 from app.my_models.cnn_torch.torchCnnModel import SimpleTorchCNNModel
 from app.signal.newTorchMegLoader import MegDataIterator
 from app.utils import my_utils as util
+from app.my_models.nn.nnModelRunner import accuracy_fn, balance_label
+
+
+
 
 class SimpleTorchCNNModelRunner:
     def __init__(self, megData, nchans, ntimes, p_drop_true=0.572):
@@ -41,11 +47,13 @@ class SimpleTorchCNNModelRunner:
             logger.error("no GPU, program exit")
             raise Exception
         
-    def run_one_epoch():
+    def process_one_task(self, i, task):
         pass
 
 
-    def train(self, epochs=10, batch_size=512, learning_rate=0.001, train_test_ratio=0.8, to_save_res=True):
+    def train(self, epochs=10, batch_size=512, learning_rate=0.001, train_test_ratio=0.8, to_save_csv=True,
+               graph_interval: int=1, print_interval: int=100):
+        
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         try:
             assert device.type == "cuda"
@@ -54,171 +62,94 @@ class SimpleTorchCNNModelRunner:
             logger.error(f"device.type: {device.type},  is not cuda! program exit!")
             raise AssertionError
 
+
+        #  --------- setup model spec   ---------#
+
         model = SimpleTorchCNNModel(self.nchans, self.ntimes).to(device)
-        criterion = nn.BCELoss()
+        loss_fn = nn.BCELoss().to(device)
         optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-        
-        for i in range(epochs):
-            self.run_one_epoch(model, criterion, )
-        
-        return
 
-        # for testing
-        ratio = (train_test_ratio, 1-train_test_ratio)
-        
-        train_size = int(ratio[0] * len(self.megData))
-        test_size = len(self.megData) - train_size
-        #not_used = len(self.megData) - train_size - test_size  # remaining data for test
+        total_epoch = 0
+        train_losses = []
+        test_losses = []
+        train_accuracies = []
+        test_accuracies = []
 
-        #train_size = int(train_test_ratio * len(self.megData))
-        #test_size = len(self.megData) - train_size
-        rand_generator = torch.Generator().manual_seed(33)      # use for fix random seede
-        train_dataset, test_dataset = random_split(self.megData, 
-                                                    lengths=[train_size, test_size], 
-                                                    generator=rand_generator)
+        final_test_pred = None
+        final_y_test = None
         
-        # for i in range(epoch):
-        #     for data in self.megData:
-        #         rand_generator = torch.Generator().manual_seed(33)
-        #         train_dataset, test_dataset = random_split(self.megData, 
-        #                                                     lengths   = [train_size, test_size], 
-        #                                                     generator = rand_generator)
-        #         DataLoader(data, batch_size=batch_size, shuffle=True)
-                
-        #         for inputs, labels in train_loader:
+        for i, task in enumerate(self.megData): # loop each task in megData -------
+
+            self.process_one_task(i, task)
+            print(f"In task {i}")
+            print("-------------------------")
+            X, y = task
+            X, y = balance_label(X, y)
+            X, y = X.to(device), y.to(device)
+
+            X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                                            test_size=(1-train_test_ratio),   # 20% test, 80% train
+                                                            random_state=25) # shffuling, making the random split reproducible
+
+   
+            train_dataset = TensorDataset(X_train, y_train)
+            dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
+            dataset_size = len(dataloader.dataset)
+            
+            
+            for epoch in range(epochs):   # loop each epoch for each task in megData ------
+                loss_item = train_acc = None
+
+                model.train()   # set model to training mode
+                for batch_num, (X_batch, y_batch) in enumerate(dataloader): # loop thru each `batch_size` events ---
                     
-        
-        # Create DataLoaders
-        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-        
-        
-        # Initialize the model, loss function, and optimizer
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        try:
-            assert device.type == "cuda"
-        except Exception as e:
-            logger.error(f"device is: {device}")
-            logger.error(f"device.type: {device.type},  is not cuda! program exit!")
-            raise AssertionError
 
-        model = SimpleTorchCNNModel(self.nchans, self.ntimes).to(device)
-        criterion = nn.BCELoss()    
-        # notes:
-        # using BCELoss expect X, y both in type float32
-        
-        # optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+                    # debugging
+                    if (i==0) and (epoch==0):
+                        logger.debug(f"shape of X_batch: {X_batch.shape}")
+                        logger.debug(f"shape of y_batch: {y_batch.shape}")
 
-        # Training loop
-        model.train()
-                # Counters for `True` and `False` labels used in training
-        true_count = 0
-        false_count = 0
 
-        for epoch in range(epochs):
-            running_loss = 0.0
-            for inputs, labels in train_loader:
+                    #   ----   training    ----  #                     
+                    outputs = model(X_batch)    # forward pass
+                    loss = loss_fn(outputs, y_batch)
 
-                # before .view, dim of inputs: [batch (10), 100(#event), 1, 208 (#chan), 41(#timepoint)]
-                # batch likely controlled by batch_size in pytorch Dataloader
-                # #event come from how __getitem__ of my torchMegLoader return the epoch (batch_size in torchMegLoader)
-                # need to flatten batch and event dim
-
-                continue
-                inputs = inputs.view(-1, 1, self.nchans, self.ntimes)
-                labels = labels.view(-1, 1)
-                inputs, labels = inputs.to(device), labels.to(device)
-
-                if self.p_drop_true > 0:
-
-                    filtered_inputs = []
-                    filtered_labels = []
-
-                    for i in range(len(labels)):
-                        label = labels[i].item()
-                        if label == 1.0:
-                            # Drop the data with 0.572 probability if the label is `True`
-                            if random.random() > self.p_drop_true:
-                                filtered_inputs.append(inputs[i])
-                                filtered_labels.append(labels[i])
-                                true_count += 1
-                        else:
-                            # Always keep the `False` labels
-                            filtered_inputs.append(inputs[i])
-                            filtered_labels.append(labels[i])
-                            false_count += 1
-
-                    if len(filtered_inputs) == 0:
-                        continue  # Skip if all data was dropped in this batch
-
-                    # Convert filtered lists back to tensors
-                    inputs = torch.stack(filtered_inputs).to(device)
-                    labels = torch.stack(filtered_labels).to(device)
-
+                    optimizer.zero_grad()   # empty gradient
+                    loss.backward()
+                    optimizer.step()
 
                 
-                logger.debug(f"inputs: {inputs.shape}")
-                logger.debug(f"labels: {labels.shape}")
+                
+                # ---    calculate loss and accuracy up to the point  --- #
+                    
+                
+                if(epoch % graph_interval== 0):
+                    model.eval()   # set model to evaluation mode
+                    y_pred = torch.round(outputs)
+                    train_acc = accuracy_fn(y_true = y_batch, y_pred = y_pred)
+                            
+                    loss_item, current = loss.item(), (batch_num + 1) * batch_size
 
-                # expexted dimension of inputs: [batch_size(#event), 1, nchans, ntimes]
-
-                #optimizer.zero_grad()   # empty gradient
-                outputs = model(inputs)
-                loss = criterion(outputs, labels)
-                loss.backward()
-                #optimizer.step()
-                running_loss += loss.item()
-            print(f'Epoch {epoch+1}/{epochs}, Loss: {running_loss/len(train_loader):.4f}')
-            print(f'Truthy samples used: {true_count}, Falsy samples used: {false_count}')
-
-        # Evaluation
-        model.eval()
-        y_pred = []
-        y_true = []
-        with torch.no_grad():
-            for inputs, labels in test_loader:
-                inputs = inputs.view(-1, 1, self.nchans, self.ntimes)
-                labels = labels.view(-1, 1)
-                inputs, labels = inputs.to(device), labels.to(device)
-                outputs = model(inputs)
-                y_pred.extend(outputs.cpu().numpy())
-                y_true.extend(labels.cpu().numpy())
-
-        y_pred = (np.array(y_pred) >= 0.5).astype(int)
-        y_true = np.array(y_true) 
-        accuracy = accuracy_score(y_true, y_pred)
-        print(f'Accuracy: {accuracy:.2f}')
-
-        # Save predictions and ground truth to DataFrame
-        prediction_df = pd.DataFrame({
-            'prediction': y_pred.flatten(),
-            'ground_truth': y_true.flatten()
-        })
-        # calculate metrics
-        prediction_df = util.add_comparison_column(prediction_df)
-        if to_save_res:
-            try:
-                prediction_df.to_csv(util.get_unique_file_name("voiced_prediction_cnn.csv", "./results"))
-            except Exception as err:
-                logger.error(err)
-                logger.error("fail to output csv, skipping output csv")
-
-            dstr = """epoch: 2\n
-                    tmin: -0.1\n
-                    tmax: 0.3\n
-                    preprocess_low_pass: 35,\n
-                    preprocess_high_pass: 180, \n
-                    data: sub 0, ses 0 , task 0\n
-                    cnn layer: 1--32 (K=1,5)--64(k=208,1)--128(linear)--1\n
-                    result: accuracy: 0.69
-                    """
-
-            util.get_eval_metrics(prediction_df, 
-                              file_name="voiced_metrics_cnn", save_path="./results", 
-                              description_str=dstr)
+                    if(epoch % print_interval== 0):
+                        print(f"Loss: {loss_item:>7f}  [{current:>5d}/{dataset_size:>5d}], Accuracy: {train_acc:>7f}%")
 
 
-# Example usage
-# Assuming you have your data in variables X and y
-# runner = SimpleTorchCNNModelRunner(X, y)
-# runner.train(epochs=10, batch_size=32, learning_rate=0.001)
+                    test_logits = model(X_test)
+                    test_pred = torch.round(test_logits)
+
+                    test_loss = loss_fn(test_logits, y_test)
+                    test_acc = accuracy_fn(y_true=y_test, y_pred=test_pred)
+                    if(epoch % print_interval== 0):
+                        print(f"Test Loss: {test_loss:>7f}, Test Accuracy: {test_acc:>7f}%\n")
+
+                    # record loss and accuracy for plot graph
+                    train_losses.append(loss_item)
+                    train_accuracies.append(train_acc)
+                    test_losses.append(test_loss.item())        # .item will convert the var to float (CPU-bound)
+                    test_accuracies.append(test_acc)
+                
+                total_epoch += 1
+
+
+
+                        
