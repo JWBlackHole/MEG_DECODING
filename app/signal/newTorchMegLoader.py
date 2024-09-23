76f0fae51a4ddbdc3b6e509e09222536bdb4a658
taliah-hl <mhlee.c@zohomail.eu>
2024-09-23 23:26:42 +0800

    correct getitem of new meg loader

--- app/signal/newTorchMegLoader.py
+++ app/signal/newTorchMegLoader.py
@@ -53,7 +53,7 @@ class MegDataIterator(Dataset):
     
     def __getitem__(self, idx):
 
-        if idx > self.totaltask: 
+        if idx >= self.totaltask: 
             raise IndexError    # define stop point of iterator
         
         
