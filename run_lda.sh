20e61877f9735cfd2200601b860adf285658a0d3
taliah-hl <taliah-1111@protonmail.com>
2024-09-23 12:54:30 +0000

    update

--- run_lda.sh
+++ run_lda.sh
@@ -3,13 +3,8 @@
 # Loop through numbers 1 to 4 for subfolder 'l'
 
 
-python main.py -o ./app/config/lda/0923/a/lda_1.json
+python main.py -o ./app/config/lda/0923/a/lda_7.json
 
     
 sleep 2
-python main.py -o ./app/config/lda/0923/a/lda_2.json
-sleep 2
-python main.py -o ./app/config/lda/0923/a/lda_5.json
-sleep 2
-python main.py -o ./app/config/lda/0923/a/lda_6.json
-
+python main.py -o ./app/config/lda/0923/a/lda_8.json
