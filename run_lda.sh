a3d6b1366525fd426d382d5cef137e60bf1f448b
taliah-hl <taliah-1111@protonmail.com>
2024-09-23 10:09:11 +0000

    add some result

--- run_lda.sh
+++ run_lda.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
 # Loop through numbers 1 to 4 for subfolder 'l'
-for i in {1..4}
-do
-    # Run the Python script with the current config file from the subfolder 'l'
-    python main.py -o ./app/config/lda/0922/l/lda_$i.json
 
-    # Wait for 2 seconds before the next iteration
-    sleep 2
-done
 
-# Loop through numbers 1 to 3 for subfolder 'o'
-for i in {2..4}
-do
-    # Run the Python script with the current config file from the subfolder 'o'
-    python main.py -o ./app/config/lda/0922/o/lda_$i.json
+python main.py -o ./app/config/lda/0923/a/lda_1.json
 
-    # Wait for 2 seconds before the next iteration
-    sleep 2
-done
+    
+sleep 2
+python main.py -o ./app/config/lda/0923/a/lda_2.json
+sleep 2
+python main.py -o ./app/config/lda/0923/a/lda_5.json
+sleep 2
+python main.py -o ./app/config/lda/0923/a/lda_6.json
+
